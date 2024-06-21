63dc0440580d08027afc710725d1e92f52e71628
Musaimin <118260340+Musaimin@users.noreply.github.com>
2024-06-21 21:45:09 +0600

    Some Debugging and Fixing
    fixed some code logics

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def load_model_dl(model_path):
     model = load_model(model_path)
     return model
 
+
 # Load the TfidfVectorizer
 with open('Models/tfidf_vectorizer.pkl', 'rb') as file:
     vectorizer = pickle.load(file)
@@ -23,6 +24,14 @@ with open('Models/tfidf_vectorizer.pkl', 'rb') as file:
 # Load the Tokenizer
 with open('Models/tokenizer.pkl', 'rb') as file:
     tokenizer = pickle.load(file)
+
+# Preprocessing Function for Deep Learning Models
+def preprocess_text_for_dl_model(text):
+    # Tokenize and pad the input text
+    sequences = tokenizer.texts_to_sequences([text])
+    padded_sequences = pad_sequences(sequences, maxlen=1000, padding='post')
+    return padded_sequences
+
 
 # Dictionary of models
 models = {
@@ -44,30 +53,27 @@ model_name = st.selectbox("Choose a model", list(models.keys()))
 
 if st.button("Classify"):
     if title and description:
-        # Combine title and description
-        text = title + " " + description
-        
+
         # Vectorize the input text for sklearn models
         if model_name in ["Linear SVM", "Logistic Regression", "Naive Bayes", "Random Forest"]:
+            text = title
             model = load_model_sklearn(models[model_name])
             text_vectorized = vectorizer.transform([text])
-            prediction = model.predict(text_vectorized)[0]
+            binary_prediction = model.predict(text_vectorized)[0]
+            print(binary_prediction)
+
         # Preprocess and predict for deep learning models
         else:
+            text = title + " " + description
             model = load_model_dl(models[model_name])
             text_preprocessed = preprocess_text_for_dl_model(text)
             prediction = model.predict(text_preprocessed)
-            prediction = np.argmax(prediction, axis=1)[0]
+            print(prediction)
+            binary_prediction = (prediction >= 0.5).astype(int)
+            print(binary_prediction)
 
         # Display result
-        result = "Fake" if prediction == 1 else "True"
+        result = "Fake" if binary_prediction == 1 else "True"
         st.write(f"The news is: {result}")
     else:
         st.write("Please enter both title and description.")
-
-# Preprocessing Function for Deep Learning Models
-def preprocess_text_for_dl_model(text):
-    # Tokenize and pad the input text
-    sequences = tokenizer.texts_to_sequences([text])
-    padded_sequences = pad_sequences(sequences, maxlen=1000, padding='post')
-    return padded_sequences
